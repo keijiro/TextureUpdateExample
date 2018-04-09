46c035b13b613f8fc271bcfba73fdcf11b53203c
Keijiro Takahashi <keijiro@gmail.com>
2018-04-09 14:30:05 +0900

    chage effect (noise -> plasma)

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,13 +6,14 @@ public class Test : MonoBehaviour
     Texture2D _texture;
     CommandBuffer _command;
 
-    [System.Runtime.InteropServices.DllImport("Noise")]
+    [System.Runtime.InteropServices.DllImport("Plasma")]
     static extern System.IntPtr GetTextureUpdateCallback();
 
     void Start()
     {
         _command = new CommandBuffer();
-        _texture = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+        _texture = new Texture2D(64, 64, TextureFormat.RGBA32, false);
+        _texture.wrapMode = TextureWrapMode.Clamp;
 
         // Set the texture to the renderer with using a property block.
         var prop = new MaterialPropertyBlock();
@@ -30,7 +31,7 @@ public class Test : MonoBehaviour
     {
         // Request texture update via the command buffer.
         _command.IssuePluginCustomTextureUpdate(
-            GetTextureUpdateCallback(), _texture, (uint)(Time.time * 30000)
+            GetTextureUpdateCallback(), _texture, (uint)(Time.time * 60)
         );
         Graphics.ExecuteCommandBuffer(_command);
         _command.Clear();
--- Plugin/Plasma.cpp
+++ Plugin/Plasma.cpp
@@ -1,8 +1,25 @@
 #include <cstdint>
+#include <cmath>
 #include "Unity/IUnityRenderingExtensions.h"
 
 namespace
 {
+    // Old school plasma effect
+    uint32_t Plasma(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t frame)
+    {
+        auto px = (float)x / width;
+        auto py = (float)y / height;
+        auto time = frame / 60.0f;
+
+        auto l = std::sinf(px * std::sinf(time * 1.3f) + std::sinf(py * 4 + time) * std::sinf(time));
+
+        auto r = (uint32_t)(std::sinf(l *  6) * 127 + 127);
+        auto g = (uint32_t)(std::sinf(l *  7) * 127 + 127);
+        auto b = (uint32_t)(std::sinf(l * 10) * 127 + 127);
+
+        return r + (g << 8) + (b << 16) + 0xff000000;
+    }
+
     // Callback for texture update events
     void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API TextureUpdateCallback(int eventID, void* data)
     {
@@ -12,10 +29,12 @@ namespace
         {
             // UpdateTextureBegin: Generate and return texture image data.
             auto params = reinterpret_cast<UnityRenderingExtTextureUpdateParams*>(data);
-            auto offs = params->userData;
+            auto frame = params->userData;
 
             uint32_t* img = new uint32_t[params->width * params->height];
-            for (auto i = 0u; i < params->width * params->height; i++) img[i] = i * 149 + offs;
+            for (auto y = 0u; y < params->height; y++)
+                for (auto x = 0u; x < params->width; x++)
+                    img[y * params->width + x] = Plasma(x, y, params->width, params->height, frame);
 
             params->texData = img;
         }
