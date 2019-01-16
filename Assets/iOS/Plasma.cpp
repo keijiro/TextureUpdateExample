4124bf1f51c97f5087949acb373d1f2477d6201c
Keijiro Takahashi <keijiro@gmail.com>
2019-01-16 14:52:54 +0900

    Update MacOS and iOS plugin code

--- Assets/iOS/Plasma.cpp
+++ Assets/iOS/Plasma.cpp
@@ -51,3 +51,37 @@ extern "C" UnityRenderingEventAndData UNITY_INTERFACE_EXPORT GetTextureUpdateCal
 {
     return TextureUpdateCallback;
 }
+
+namespace
+{
+    // Callback for texture update events
+    void TextureUpdateCallbackV2(int eventID, void* data)
+    {
+        auto event = static_cast<UnityRenderingExtEventType>(eventID);
+
+        if (event == kUnityRenderingExtEventUpdateTextureBeginV2)
+        {
+            // UpdateTextureBegin: Generate and return texture image data.
+            auto params = reinterpret_cast<UnityRenderingExtTextureUpdateParamsV2*>(data);
+            auto frame = params->userData;
+
+            uint32_t* img = new uint32_t[params->width * params->height];
+            for (auto y = 0u; y < params->height; y++)
+                for (auto x = 0u; x < params->width; x++)
+                    img[y * params->width + x] = Plasma(x, y, params->width, params->height, frame);
+
+            params->texData = img;
+        }
+        else if (event == kUnityRenderingExtEventUpdateTextureEndV2)
+        {
+            // UpdateTextureEnd: Free up the temporary memory.
+            auto params = reinterpret_cast<UnityRenderingExtTextureUpdateParamsV2*>(data);
+            delete[] reinterpret_cast<uint32_t*>(params->texData);
+        }
+    }
+}
+
+extern "C" UnityRenderingEventAndData UNITY_INTERFACE_EXPORT GetTextureUpdateCallbackV2()
+{
+    return TextureUpdateCallbackV2;
+}
